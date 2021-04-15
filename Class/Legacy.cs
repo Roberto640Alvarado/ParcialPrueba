224cf01d633e85c3bacaf1e0741c96745a005499
Roberto640Alvarado <00176620@uca.edu.sv>
2021-04-14 18:53:08 -0600

    End TaskR

--- Class/Legacy.cs
+++ Class/Legacy.cs
@@ -4,10 +4,17 @@ namespace ParcialMamalon.Class
 {
     public class Legacy : Phone, IcellLegacy
     {
-        //
+        public Legacy(double currentBalance ) : base(currentBalance)
+        {    
+        }
+
         public void RechargeBalance()
         {
-            throw new NotImplementedException();
+            Console.Write("Introduzca el saldo a recargar:");
+            double balance = Double.Parse(Console.ReadLine());
+            currentBalance += balance;
+            Console.WriteLine($"Se ha recargado {currentBalance} a su Telefono");
+            //Movements.Add(new Movement(balance , "Recargar de saldo"));
         }
     }
 }
