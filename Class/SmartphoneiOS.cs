a48bbcf6c2d32bb76db3145d31e4fce156be42c8
EA03 <00175420@uca.edu.sv>
2021-04-14 20:09:33 -0600

    End BranchB

--- Class/SmartphoneiOS.cs
+++ Class/SmartphoneiOS.cs
@@ -44,20 +44,128 @@ namespace ParcialMamalon.Class
 
             }
         }
-
+        
+        //metodo para recargar saldo 
         public void RechargeBalances()
         {
-            throw new NotImplementedException();
+            double balanceE;
+            Console.WriteLine("Cunato saldo desea recargar?");
+
+            do //validacion para comprobar que el saldo no sea negativo
+            {
+
+             balanceE = Convert.ToDouble(Console.ReadLine());
+
+            }while(balanceE <= 0);
+            
+            currentBalance += balanceE;
+            Console.WriteLine("El saldo ha sido agreditado a su telefono");
+            //Movements.Add(new Movement(balanceE, $"Recarga de saldo"));
         }
 
+        //Metodo para recargar para navegar
         public void ReloadNavigation()
         {
-            throw new NotImplementedException();
+          bool status = true;
+          Console.ReadLine("----------RECARGAR PARA NAVEGACION GENERAL----------");
+          Console.WriteLine("1) Recargar Redes Socieles. \n" +
+          "2) Recargar Navegacion General. \n"+
+          "Opcion: ");
+          int option = int32.Parse(Console.ReadLine());
+
+          switch(status)
+          {
+              case 1: MenuRed(); break;
+              case 2: MenuWeb(); break;
+              case 3: break;
+              default: Console.WriteLine("Opcion erronea"); break;
+          }
         }
 
+        //Metodo para recargar canciones
         public void ReloadSongs()
         {
-            throw new NotImplementedException();
+             int option;
+
+           do
+           {    Console.WriteLine("-----Recargar Canciones-----\n" + 
+                "1) 200 canciones en Spotify -$3.00\n" +
+                "2) Salir\n" +
+                "Opción elegida: "); 
+                option = int32.Parse(Console.ReadLine());
+
+                switch(option)
+                {
+                    case 1:
+                    if(currentBalance >= 3)
+                    {
+                        Console.WriteLine("Se han agregado 200 canciones de Spotify, a disfrutar.....");
+                        ncancion += 200;
+                        //Movements.Add(new Movement(3,"Agrego 200 canciones a Spotify));
+                    }
+                    else
+                    Console.WriteLine("Su saldo es insuficiente :("); break;
+
+                    case 2: break;
+                    default: Console.WriteLine("Opcion erronea"); break;
+                }
+           }while(option != 1);
+        }
+        
+        //Menu para recargar redes
+        public void MenuRed()
+        {
+            boll status = true;
+            while(status)
+            {
+                Console.WriteLine("----------RECARGAR REDES SOCIALES----------");
+                Console.WriteLine("1) 500MB para redes sociales \n "+
+                "2) Regresar.");
+                int option = int32.Parse(Console.ReadLine());
+
+                switch(status)
+                {
+                    case 1:
+                    if(currentBalance >= 4)
+                    {
+                        Console.WriteLine("Se han agregado 500MB para redes sociales ");
+                        dataRedes += 500;
+                        //Movements.Add(new Movement(4,"Se agregaron 500MB para redes sociales));
+                    }
+                    else
+                    Console.WriteLine("Su saldo es insuficiente"); break;
+                    case 2: break;
+                    default: Console.WriteLine("Opcion invalida"); break;
+                }
+            }
+        }
+        //Menu para recargar navegacion general
+        public void MenuWeb()
+        {
+            boll status = true;
+            while(status)
+            {
+                Console.WriteLine("----------RECARGAR NAVEGACION----------");
+                Console.WriteLine("1) 500MB Navegcion general \n "+
+                "2) Regresar.");
+                int option = int32.Parse(Console.ReadLine());
+
+                switch(status)
+                {
+                    case 1:
+                    if(currentBalance <= 5)
+                    {
+                        Console.WriteLine("Se han agregado 500MB para navegacion general ");
+                        dataNavegation += 500;
+                        //Movements.Add(new Movement(5,"Se agregaron 500MB para navegacion general));
+                    }
+                    else
+                    Console.WriteLine("Su saldo es insuficiente"); break;
+                    case 2: break;
+                    default: Console.WriteLine("Opcion invalida"); break;
+                }
+
+            }
         }
     }
 }
